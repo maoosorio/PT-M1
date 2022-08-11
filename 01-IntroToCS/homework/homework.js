68db3711afc6e4c5e7a67aa6cebbfd6d0fc24a6d
Mario <isc_osorio@hotmail.com>
2022-08-11 12:41:35 -0500

    primer tarea

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -1,17 +1,29 @@
 'use strict'
 
 function BinarioADecimal(num) {
-  // tu codigo aca
-
+	// tu codigo aca
+	var suma = 0;
+	for (let i = 0; i < num.length; i++) {
+		suma += num[i] * (Math.pow(2, num.length - 1 - i));
+	}
+	return suma;
 }
 
 function DecimalABinario(num) {
-  // tu codigo aca
-
+	// tu codigo aca
+	let binario = 0;
+	let sobrante, i = 1;
+	while (num != 0) {
+		sobrante = num % 2;
+		num = Math.floor(num / 2);
+		binario += sobrante * i;
+		i = i * 10;
+	}
+	return binario.toString();
 }
 
 
 module.exports = {
-  BinarioADecimal,
-  DecimalABinario,
+	BinarioADecimal,
+	DecimalABinario,
 }
